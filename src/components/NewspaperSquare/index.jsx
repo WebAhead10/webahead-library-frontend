a5b297caea3e14c326cea89b5b624b92828b1c60
--replace-all <mario.saliba98@gmail.com>
2021-09-06 10:46:19 +0300

    final push

--- src/components/NewspaperSquare/index.jsx
+++ src/components/NewspaperSquare/index.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react"
 import "./style.css"
 
 function NewspaperSquare(props) {
-    return (
-        <div>
-            <div className="news" >
-                <a className="click" href="/hassan">
-                <img className="pic" src={props.logo} alt="" style={props.style} />
-            <b className="publisherName" >{props.name}</b>
-                </a>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="news">
+        <a className="click" href="/hassan">
+          <img className="pic" src={props.logo} alt="" style={props.style} />
+          <b className="publisherName">{props.name}</b>
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default NewspaperSquare
--- src/pages/Newspaper/index.jsx
+++ src/pages/Newspaper/index.jsx
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import NewspaperSquare from '../../components/NewspaperSquare/index';
-import axios from 'axios';
+import React, { useState, useEffect } from "react"
+import NewspaperSquare from "../../components/NewspaperSquare/index"
+import axios from "axios"
 import "./style.css"
-const PORT = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL
 
 function Newspaper() {
+  const [publisherArr, setPublisherArr] = useState([])
+  const [error, setError] = useState("")
 
-    const [publisherArr, setPublisherArr] = useState([])
-    const [error, setError] = useState("")
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/publishers`)
+      .then((res) => {
+        if (!res.data.success) {
+          setError("Failed")
+          return
+        }
 
-    useEffect(() => { 
-        axios.get(`${PORT}/publishers`)
-        .then((res) => {
-            setPublisherArr(res.data.publisher)
-            console.log(res.data.publisher)
-        })
-        .catch((error) => {
-            setError(error.message);
-        })
-    }, [])
+        setPublisherArr(res.data.publisher)
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+  }, [])
 
-    return (
-            <div className="news-container">
-                {publisherArr.map((publisher, index) => <NewspaperSquare name={publisher.name}
-                    style={{ objectFit: (index + 1) % 4 === 0 ? 'cover' : '',
-                    background: (index + 1) % 5 === 0 ? 'white' : '',
-                }}
-                    logo={publisher.logo}
-                    key={publisher.id}
-                />)}
-                
+  return (
+    <div className="news-container">
+      {publisherArr.map((publisher, index) => (
+        <NewspaperSquare
+          name={publisher.name}
+          style={{
+            objectFit: (index + 1) % 4 === 0 ? "cover" : "",
+            background: (index + 1) % 5 === 0 ? "white" : "",
+          }}
+          logo={publisher.logo}
+          key={publisher.id}
+        />
+      ))}
 
-                <span>{error}</span>
-
-            </div>
-
-
-    )
-
-
+      <span>{error}</span>
+    </div>
+  )
 }
-
-
 
 export default Newspaper
