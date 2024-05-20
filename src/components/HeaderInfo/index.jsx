03f62e6cb7b09afd5f6a5805a11b5465997d1fbb
Mario Saliba <mario.saliba98@gmail.com>
2024-05-21 01:22:32 +0300

    .

--- src/components/HeaderInfo/index.jsx
+++ src/components/HeaderInfo/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.css'
 import { Link } from 'react-router-dom'
 import { UserOutlined } from '@ant-design/icons'
-import { useRecoilValue } from 'recoil'
+import { useRecoilState } from 'recoil'
 import { userAtom } from 'utils/recoil/atoms'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'utils/axios'
@@ -12,14 +12,13 @@ function HeaderInfo(props) {
   const { refetch } = useQuery(['logout'], () => axios.get('/user/logout'), { enabled: false })
   const queryClient = useQueryClient()
   const history = useHistory()
+  const [user, setUser] = useRecoilState(userAtom)
 
   const signout = () => {
     refetch()
-    window.location.reload()
     queryClient.invalidateQueries('user')
+    setUser({})
   }
-
-  const user = useRecoilValue(userAtom)
 
   return (
     <div className="headerInfo">
