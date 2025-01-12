41de1327e972e4c106d069aa08db8c844872436a
fridabu <f0504070002@gmail.com>
2025-01-12 19:30:06 +0200

    update category image size

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -160,7 +160,7 @@ function HomePage() {
             <MainCategorySquare
               name={category.name}
               style={{
-                objectFit: (index + 1) % 4 === 0 ? 'cover' : 'fill',
+               //  objectFit: (index + 1) % 4 === 0 ? 'cover' : 'fill',
                 background: (index + 1) % 5 === 0 ? 'white' : ''
               }}
               logo={category.logo}
